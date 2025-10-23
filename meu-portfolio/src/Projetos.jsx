fc42ee0b4512c6f6b538d081332cab54d950a37f
Paulo Henrique De Souza Pantaleão <p248437@ftad.ft.unicamp.br>
2025-10-23 15:51:36 -0300

    Atualizacao projetos

--- meu-portfolio/src/Projetos.jsx
+++ meu-portfolio/src/Projetos.jsx
@@ -8,14 +8,17 @@ function Projetos(){
                     <h3> Projeto 1</h3>
                     <p> Descrição do projeto 1</p>
                 </div>
-            </div>
-            <div className="projetos-grid">
                 <div className="projeto-card">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_SYxURXMPP0P05aM7flfDw48ucXUIRiCabw&s" alt="Projeto 2" />
                     <h3> Projeto 2</h3>
                     <p> Descrição do projeto 2</p>
                 </div>
-            </div>
+                <div className="projeto-card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_SYxURXMPP0P05aM7flfDw48ucXUIRiCabw&s" alt="Projeto 3" />
+                    <h3> Projeto 3</h3>
+                    <p> Descrição do projeto 3</p>
+                </div>
+            </div>  
         </section>
     );
 }
